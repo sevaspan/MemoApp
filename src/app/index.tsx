1bbc7f37d8b0b4663947f9325b7a8b0198fb220b
對馬一希 <kazuki@tsushimakazukinoMacBook-Pro.local>
2026-01-18 16:56:42 +0900

    Install expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "react";
+import { Text, View } from "react-native";
+
+const Index = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default Index;
